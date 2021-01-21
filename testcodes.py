6e34d56f0cc54b8f50a7c9c8445b756e4a5e1386
ChuaKaiMing <73737339+ChuaKaiMing@users.noreply.github.com>
2021-01-21 20:15:17 +0800

    Add files via upload
    Newest code

--- testcodes.py
+++ testcodes.py
@@ -1,34 +1,42 @@
-AllVar = ['Intro','NameOfMyFavoriteSong','PublishedYear','Artist','Conti1','Conti2','DurationInSec','Conti3','Conti4','Genre','WhereYouCanEnjoy']
-EachVarValue = ['My song of choice is : ','Beautiful Sunday', 1972 , 'Daniel Boone','Although this song is quite old for now, listening to this song is quite recommended.','It takes around','188','seconds.','Genre :','Rock.','You can find this song in YouTube, Spotify, Joox and any other applications that fuctions to play music.']
-songdictionary = {}
+AllVar = ['Intro','NameOfMyFavoriteSong','PublishedYear','Artist','DurationInSec','Genre','Desciption','WhereYouCanEnjoy']
+EachVarValue = ['My song of choice is : ','Beautiful Sunday', 1972 , 'Daniel Boone',188,'Rock','Although this song is quite old for now, listening to this song is quite recommended.','You can find this song in YouTube, Spotify, Joox and any other applications that fuctions to play music.']
+songdictionary = {} #Normally the dictionary will not keep the key in the same order, but adding things into the dictionary wil keep the key in ascending value
 counterforlist2 = 0
 
 for a in AllVar:
     songdictionary[a] = EachVarValue[counterforlist2]
     counterforlist2 += 1
 
-print('Hello, Welcome!')
-
-for b in AllVar:
-    if b == AllVar[1:4]:
-        print('Do you want to guess what is the', AllVar[b] + '?')
-        reply = str(input('Yes or No? 5 chances for each questions. Type right down here\n'))
-        if reply == 'Yes' or reply == 'yes':
-            for c in range(1,6):
-                answer = str(input('Chance {} / Your answer: '.format(c)))
-                if answer == EachVarValue[1] or answer == EachVarValue[2] or answer == EachVarValue[3]:
-                    print('Correct!')
-                elif c == 5:
-                    print('The answer is:',EachVarValue[b])
-                else:
-                    print('Incorrect answer.')
-        elif reply == 'No' or reply == 'no':
-            print(AllVar[0:b+1])
-        else:
-            reply2 = str(input('Skip all?\n'))
-            if reply2 == 'skip all' or reply2 == 'Skip all':
-                print(songdictionary)
-                #Until here the code is working absolutely fine
-    else:
-        print(EachVarValue[0:b])
-print(songdictionary)
+print('Hello, welcome!')
+userinput = str(input('Do you want to guess my favorite song? Y/N\n'))
+if userinput == 'Y':
+    for loop in range(10):
+        answer, chance = str(input('Your answer: ')), print('Chances left:',9-loop)
+        if answer == EachVarValue[1]:
+            print('Absolutely correct!')
+        elif answer != EachVarValue[1] and loop < 9:
+            print('Uh oh, it is a wrong guess.')
+        elif answer != EachVarValue[1] and loop == 9:
+            print('Your chances ran out. The answer is:', EachVarValue[1]+'!')
+elif userinput == 'N':
+    for loop2 in songdictionary:
+        print(loop2,songdictionary[loop2])
+        exit
+else:
+    exit
+userinput2 = str(input('\nDo you want to guess the characteristics of my favorite song? (There are 4 of them) Y/N\n')) #Here is the error
+if userinput2 == 'Y':
+    for loop3 in range(2,6):
+        for loop4 in range(10):
+            answer2 = str(input('What is the',EachVarValue[loop3],'of Beautiful Sunday?'))
+            if answer2 == EachVarValue[loop3]:
+                print('Absolutely correct!')
+            elif answer2 != EachVarValue[loop3] and loop4 <= 9:
+                print('Uh oh, it is a wrong guess.')
+            else:
+                print('Your chances ran out. The answer is:', EachVarValue[1]+'!')
+elif userinput2 == 'N':
+    print(songdictionary)
+else:
+    print('Bye')
+print('\nThank you for participating!')
